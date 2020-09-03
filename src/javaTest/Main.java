e981b9bcbddb8420900c5e4615fe9d143b07c453
monkey999por <yururi1122@gmail.com>
2020-09-04 02:18:54 +0900

    ファイルの出力のサンプル

--- src/javaTest/Main.java
+++ src/javaTest/Main.java
@@ -1,14 +1,46 @@
 package javaTest;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Main m = new Main();
-		
-		
+		System.out.println("Start");
+		createFile();
+		System.out.print("End");
+
 	}
 
 	static void createFile() {
+		//Windowsの環境変数 USERPROFILEを取得
+		String USERPROFILE = System.getenv("UserProfile");
+		
+		//既存ファイルを削除して空のファイルを再作成する.
+		//ディレクトリがなければ作る
+		File file = new java.io.File(USERPROFILE + "\\Downloads\\out_temp\\test.properties");
+		file.mkdirs();
+		if (file.canExecute())
+			file.delete();
+		
+		try {
+			file.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		//プロパティを作成して書き込む。
+		try (OutputStream out = new FileOutputStream(file)) {
+			Properties prop = new Properties();
+			prop.setProperty("tanaka", "taro");
+			prop.setProperty("yamada", "hanako");
+			prop.store(out, null);			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
